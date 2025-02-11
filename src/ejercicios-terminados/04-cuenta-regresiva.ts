7279ae8e5d375cd22dcd41ad3267b7a181a6042a
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2025-02-11 09:45:31 -0500

    Ejercicio cuenta regresiva

---
+++ src/ejercicios-terminados/04-cuenta-regresiva.ts
@@ -0,0 +1,34 @@
+import {filter, interval, take} from 'rxjs';
+import {map} from "rxjs/operators";
+
+/**
+ * Ejercicio: Realizar una cuenta regresiva
+ * empezando de 7
+ */
+
+// Salida esperada ===
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+
+(() =>{
+
+    const inicio = 7;
+    const countdown$ = interval(700).pipe(
+        // Usar los operadores necesarios
+        // para realizar la cuenta regresiva
+        take(inicio + 1),
+        map( num => inicio - num),
+    );
+
+    // No tocar esta línea ==================
+    countdown$.subscribe( console.log ); // =
+    // ======================================
+
+})();
+
